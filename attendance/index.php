ca816b09f5c89b2cd7d7bce70593739c65922d78
Joao Vitor Batistella <joaovitor.batistella.ifrs@gmail.com>
2021-08-07 15:46:58 -0300

    [First Commit]-07082021-15-46

---
+++ attendance/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
+  <title>Atendimento</title>
+</head>
+<body>
+  <div class="container">
+    <nav class="row">
+      <ul class="ul">
+        <li><a href="/">Home</a></li>
+        <li><a href="/attendance/">Atendimento</a></li>
+        <li><a href="/connection/">BD</a></li>
+      </ul>
+    </nav>
+  </div>
+</body>
+</html>
